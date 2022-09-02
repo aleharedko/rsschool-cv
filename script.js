0764e2986bac6ddcc84ff3a7c0538a68f030bfe2
Aliakse Redzko <aleha.redko@gmail.com>
2022-09-02 11:01:29 +0300

    feat: fix aside, add more feacher in humburger menu, add a littel code is js.js

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+/*<script> function popup(url)
+{
+  window.open(url, 'new', 'width=800,height=600,scrollbars=no,menubar=no,status=no,toolbar=no');
+}
+</script>
+<a onClick="popup('http://адрес HTML5-презентации')">текст ссылки</a>*/
