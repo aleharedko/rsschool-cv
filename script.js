8ee9ad160ee10cc0bc5db06d273191b96914b361
Aliakse Redzko <aleha.redko@gmail.com>
2022-10-18 14:17:42 +0300

    fix: add more code in humb menu

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
 "use strict"
-// Берём кнопку вперёд
-let btnRight = document.querySelector(".btnRight");
-let btnLeft = document.querySelector(".btnLeft");
-// Берём слайды
-let slides = document.querySelectorAll(".imgSlider");
- 
-let i = 0;
- 
-/* Объявляем событие нажатия на кнопку вперёд
-btnRight.addEventListener("click", function () {
-    // Увеличиваем переменную i
-    ++i
-    // Условие если переменная i больше или равна количеству слайдов
-    if (i >= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    }
-})
-
-btnLeft.addEventListener("click", function () {
- 
-})*/
 
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.btnRight').addEventListener('click', function(){
     offset = offset + 760;
-    if (offset > 2280){
+    if (offset > 3800){
         offset = 0;
     }
     sliderLine.style.left = - offset + 'px';
@@ -45,7 +14,14 @@ document.querySelector('.btnRight').addEventListener('click', function(){
 document.querySelector('.btnLeft').addEventListener('click', function(){
     offset = offset - 760;
     if (offset < 0){
-        offset = 2280;
+        offset = 3800;
     }
     sliderLine.style.left = - offset + 'px';
+})
+
+const hamb = document.querySelector('#hamb');
+const popup = document.querySelector('.popup');
+
+hamb.addEventListener('click', function() {
+    
 })
